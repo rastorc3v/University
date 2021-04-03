d4b41dade7204510bda9aacdf9f5f9e27c3db81b
RasssTor <4etkuu-poz@mail.ru>
2021-04-03 16:58:53 +0300

    create fields, getters and setters

--- java/practice2/src/by/gsu/asoilab/Material.java
+++ java/practice2/src/by/gsu/asoilab/Material.java
@@ -1,4 +1,22 @@
 package by.gsu.asoilab;
 
 public class Material {
+    private String name;
+    private int density;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDensity() {
+        return density;
+    }
+
+    public void setDensity(int density) {
+        this.density = density;
+    }
 }
