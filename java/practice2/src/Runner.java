aafa4613e48faa1fc9e34fea00863716d47f4001
RasssTor <4etkuu-poz@mail.ru>
2021-04-03 17:38:46 +0300

    create wire object and show properties

--- java/practice2/src/Runner.java
+++ java/practice2/src/Runner.java
@@ -1,5 +1,8 @@
+import by.gsu.asoilab.Item;
+
 public class Runner {
     public static void main(String[] args) {
-
+        Item wire = new Item(7850, "wire", "steel", (float) 0.03);
+        System.out.println(wire.toString());
     }
 }
