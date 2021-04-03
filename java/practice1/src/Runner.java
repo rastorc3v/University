a2c39aef4ce6a1881d994b3cea4aa340d5fa565d
RasssTor <4etkuu-poz@mail.ru>
2021-04-03 15:33:22 +0300

    refactor business trip runner class

--- java/practice1/src/Runner.java
+++ java/practice1/src/Runner.java
@@ -12,6 +12,7 @@ public class Runner {
                 new BusinessTrip(500, "Pam", 1100, 4),
                 new BusinessTrip(100)
         };
+
         for (BusinessTrip trip : trips) {
             if (trip != null) {
                 trip.show();
@@ -20,9 +21,12 @@ public class Runner {
                 System.out.println("This is null item!\n-----------------");
             }
         }
+
         trips[trips.length - 1].setTransport(2000);
         int duration = trips[0].getDays() + trips[1].getDays();
+
         System.out.println("Duration = " + duration);
+
         for (BusinessTrip trip : trips) {
             if (trip != null) {
                 System.out.println(trip.toString());
@@ -31,28 +35,37 @@ public class Runner {
                 System.out.println("This is null item!\n-----------------");
             }
         }
+
         int totalSum = 0;
+
         for (BusinessTrip trip : trips) {
             if (trip != null) {
                 totalSum += trip.getTotal();
             }
         }
+
         System.out.println("Total sum = " + totalSum);
+
         int[] totalsList = new int[trips.length];
+
         for (int i = 0; i < trips.length; i++) {
             if (trips[i] != null) {
                 totalsList[i] = Objects.requireNonNull(trips[i]).getTotal();
             }
         }
+
         System.out.println(totalsList[2]);
+
         int maxTotal = 0;
         String accountNameWithMaxExpenses = "";
+
         for (int i = 0; i < totalsList.length; i++) {
             if (totalsList[i] > maxTotal) {
                 maxTotal = totalsList[i];
                 accountNameWithMaxExpenses = Objects.requireNonNull(trips[i]).getAccount();
             }
         }
+
         System.out.println(accountNameWithMaxExpenses);
     }
 }
