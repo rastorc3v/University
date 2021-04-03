e7201219e6cf59e5415d93333e471bb1d58c04d0
RasssTor <4etkuu-poz@mail.ru>
2021-04-03 13:41:15 +0300

    create icecream name field create getter and setter

--- java/IDZ1/src/by/gsu/pms/Icecream.java
+++ java/IDZ1/src/by/gsu/pms/Icecream.java
@@ -1,13 +1,15 @@
 package by.gsu.pms;
 
 public class Icecream {
+    private String name;
     private boolean isChocolate;
     private float fatPercentage;
 
     public Icecream() {
     }
 
-    public Icecream(boolean isChocolate, float fatPercentage) {
+    public Icecream(String name, boolean isChocolate, float fatPercentage) {
+        this.name = name;
         this.isChocolate = isChocolate;
         this.fatPercentage = fatPercentage;
     }
@@ -27,6 +29,10 @@ public class Icecream {
     public void setFatPercentage(float fatPercentage) {
         this.fatPercentage = fatPercentage;
     }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getName() { return this.name; }
 
     public String toString() {
         return "Is chocolate - " + isChocolate + " fat percentage - " + fatPercentage;
