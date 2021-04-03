718cdea87bc6bdc5900de3b782a3bd01dd2b6795
RasssTor <4etkuu-poz@mail.ru>
2021-04-03 17:31:42 +0300

    create constructors and methods

--- java/practice2/src/by/gsu/asoilab/Item.java
+++ java/practice2/src/by/gsu/asoilab/Item.java
@@ -1,9 +1,53 @@
 package by.gsu.asoilab;
 
-public class Item extends  Material {
+public class Item extends Material {
     private String name;
     private String material;
     private float volume;
 
+    public Item(String name, String material, float volume) {
+        this.name = name;
+        this.material = material;
+        this.volume = volume;
+    }
 
+    public Item(int density, String name, String material, float volume) {
+        super(material, density);
+        this.name = name;
+        this.material = material;
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public float getVolume() {
+        return volume;
+    }
+
+    public void setVolume(float volume) {
+        this.volume = volume;
+    }
+
+    private float getMass() {
+        return volume * getDensity();
+    }
+
+    @Override
+    public String toString() {
+        return name + ';' + super.toString() + ';' + volume + ';' + getMass();
+    }
 }
