f7338ef9ffd83903e77c948f555fd56de6ff450b
RasssTor <4etkuu-poz@mail.ru>
2021-04-04 01:19:40 +0300

    create getters and setters in CommodityPurchase class

--- java/practice2/task1/src/by/gsu/pms/CommodityPurchase.java
+++ java/practice2/task1/src/by/gsu/pms/CommodityPurchase.java
@@ -14,4 +14,28 @@ public class CommodityPurchase {
         this.purchasedUnits = purchasedUnits;
     }
 
+    public String getCommodityName() {
+        return commodityName;
+    }
+
+    public void setCommodityName(String commodityName) {
+        this.commodityName = commodityName;
+    }
+
+    public float getCommodityPrice() {
+        return commodityPrice;
+    }
+
+    public void setCommodityPrice(float commodityPrice) {
+        this.commodityPrice = commodityPrice;
+    }
+
+    public int getPurchasedUnits() {
+        return purchasedUnits;
+    }
+
+    public void setPurchasedUnits(int purchasedUnits) {
+        this.purchasedUnits = purchasedUnits;
+    }
+
 }
