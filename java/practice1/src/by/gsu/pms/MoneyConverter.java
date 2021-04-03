335cd7b1c05df336e14a18d6e536684611c93d2b
RasssTor <4etkuu-poz@mail.ru>
2021-04-03 16:01:00 +0300

    implement int2string method in MoneyConverter

--- java/practice1/src/by/gsu/pms/MoneyConverter.java
+++ java/practice1/src/by/gsu/pms/MoneyConverter.java
@@ -1,11 +1,26 @@
 package by.gsu.pms;
 
 public class MoneyConverter {
-    public String int2string() {
-        return "";
+    // how much kopeck in rubble (example: 100)
+    int divider = 100;
+    String bigDenominationName = "руб.";
+    String smallDenominationName = "коп.";
+
+    public MoneyConverter() { }
+
+    public MoneyConverter(int divider, String bigDenominationName, String smallDenominationName) {
+        this.divider = divider;
+        this.bigDenominationName = bigDenominationName;
+        this.smallDenominationName = smallDenominationName;
     }
 
-    public String float2string() {
+    public String int2string(int moneyEquivalent) {
+        int bigDenomination = moneyEquivalent / this.divider;
+        int smallDenomination = moneyEquivalent % this.divider;
+        return bigDenomination + bigDenominationName + smallDenomination + smallDenominationName;
+    }
+
+    public String float2string(float moneyEquivalent) {
         return "";
     }
 }
