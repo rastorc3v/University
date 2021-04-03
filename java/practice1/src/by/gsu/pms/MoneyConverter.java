bc8dcbc12221fb9e426baaef41e58bffe63a8695
RasssTor <4etkuu-poz@mail.ru>
2021-04-03 15:45:37 +0300

    create MoneyConverter class with int2string and float2string methods

---
+++ java/practice1/src/by/gsu/pms/MoneyConverter.java
@@ -0,0 +1,11 @@
+package by.gsu.pms;
+
+public class MoneyConverter {
+    public String int2string() {
+        return "";
+    }
+
+    public String float2string() {
+        return "";
+    }
+}
