aac6a81116bfd5580e674b179134dc51f671876c
RasssTor <4etkuu-poz@mail.ru>
2021-04-04 01:16:50 +0300

    create constructors in CommodityPurchase class

--- java/practice2/task1/src/by/gsu/pms/CommodityPurchase.java
+++ java/practice2/task1/src/by/gsu/pms/CommodityPurchase.java
@@ -4,4 +4,14 @@ public class CommodityPurchase {
     private String commodityName;
     private float commodityPrice;
     private int purchasedUnits;
+
+    public CommodityPurchase() {
+    }
+
+    public CommodityPurchase(String commodityName, float commodityPrice, int purchasedUnits) {
+        this.commodityName = commodityName;
+        this.commodityPrice = commodityPrice;
+        this.purchasedUnits = purchasedUnits;
+    }
+
 }
