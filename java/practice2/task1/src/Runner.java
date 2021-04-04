05fcf24e863a7d04e049fb1288f0feef6000805a
RasssTor <4etkuu-poz@mail.ru>
2021-04-05 01:12:20 +0300

    finish algorithm

--- java/practice2/task1/src/Runner.java
+++ java/practice2/task1/src/Runner.java
@@ -3,15 +3,35 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
+import by.gsu.pms.CommodityPurchase;
+import by.gsu.pms.PurchasesFactory;
+
+
 public class Runner {
     public static void main(String[] args) throws IOException {
         String fileName = "java/practice2/task1/src/in.txt";
         Path path = Paths.get(fileName);
         Scanner scanner = new Scanner(path);
         scanner.useDelimiter(System.getProperty("line.separator"));
-        while(scanner.hasNext()){
-            System.out.println(scanner.next());
+        PurchasesFactory purchasesFactory = new PurchasesFactory();
+        CommodityPurchase[] purchasesArray = new CommodityPurchase[6];
+        float maxCost = 0;
+        float totalCost = 0;
+        boolean allPurchaseEqual = true;
+        for(int i = 0; scanner.hasNext() && i < purchasesArray.length; i++){
+            CommodityPurchase purchase = purchasesFactory.getClassFromFactory(scanner);
+            purchasesArray[i] = purchase;
+            System.out.println(purchase.toString());
+            maxCost = Math.max(purchase.getCost(), maxCost);
+            totalCost += purchase.getCost();
+            if (i > 0 && Boolean.FALSE.equals(purchase.equals(purchasesArray[i - 1]))) {
+                allPurchaseEqual = false;
+            }
         }
+        System.out.println("Max cost - " + maxCost);
+        System.out.println("Total cost - " + totalCost);
+        System.out.println("All purchases are equal - " + allPurchaseEqual);
         scanner.close();
+
     }
 }
