058342b7592e2b9302d242dfa29004e551a528ec
RasssTor <4etkuu-poz@mail.ru>
2021-04-04 15:38:05 +0300

    create DynamicDiscountCommodityPurchase class and override necessary method

---
+++ java/practice2/task1/src/by/gsu/pms/DynamicDiscountCommodityPurchase.java
@@ -0,0 +1,28 @@
+package by.gsu.pms;
+
+public class DynamicDiscountCommodityPurchase extends CommodityPurchase {
+    static final int MIN_PURCHASE_UNITS = 10;
+    private float discount = (float) 5.125;
+
+    public float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(float discount) {
+        this.discount = discount;
+    }
+
+    public DynamicDiscountCommodityPurchase(String commodityName, float commodityPrice, int purchasedUnits, float discount) {
+        super(commodityName, commodityPrice, purchasedUnits);
+        this.discount = discount;
+    }
+
+    @Override
+    public float getCost() {
+        if (getPurchasedUnits() > MIN_PURCHASE_UNITS) {
+            return getCommodityPrice() * getPurchasedUnits() * (1 - discount/100);
+        } else {
+            return super.getCost();
+        }
+    }
+}
